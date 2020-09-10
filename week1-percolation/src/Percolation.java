bfe0ce4e88232df01c9723f23098ac2d0236b790
tobias <tobias@dsaid.gov.sg>
2020-09-11 02:38:48 +0800

    Add percolation and percolationstats

--- week1-percolation/src/Percolation.java
+++ week1-percolation/src/Percolation.java
@@ -1,25 +1,87 @@
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
-    private int[] id;
-    private int virtualTop = 0; // set this to 0 because top-left corner of the grid is (1,1)
-    private int virtualBottom = -1;
+    private boolean[] id;
+    private WeightedQuickUnionUF connectionGrid;
+    private int gridSize;
+
+    // because top-left corner of the grid is (1,1)
+    private final int virtualTop = 0;
+    private final int virtualBottom;
 
     // creates n-by-n grid with all sites initially blocked
     public Percolation(int n) {
         if (n <= 0) throw new IllegalArgumentException("n must be positive"); // illegal argument
 
-        id = new int[n];
-        for (int i = 0; i < n; i++) {
-            id[i] = i; // set id of each obj to itself
-        }
+        gridSize = n;
+        virtualBottom = gridSize + 1;
+        id = new boolean[n + 1]; // use default value of 0 to set all sites to blocked
+        connectionGrid = new WeightedQuickUnionUF(gridSize + 1); // account for virtualBottom too
     }
 
     // opens the site (row, col ) if it is not open already
     public void open(int row, int col) {
+        int singleArrayRep = convertToSingleArray(row, col);
+        if (!id[singleArrayRep]) {
+            // site is currently closed
+            id[singleArrayRep] = true;
+            // check all sites around it and union them
 
+            // above
+            if (row == 1) {
+                connectionGrid.union(singleArrayRep, virtualTop); // union with virtualTop
+            } else {
+                connectionGrid.union(singleArrayRep, convertToSingleArray(row - 1, col));
+            }
+
+            // left
+            if (col != 1) {
+                connectionGrid.union(singleArrayRep, convertToSingleArray(row, col - 1));
+            }
+
+            // right
+            if (col != gridSize) {
+                connectionGrid.union(singleArrayRep, convertToSingleArray(row, col + 1));
+            }
+
+            // bottom
+            if (row == gridSize) {
+                connectionGrid.union(singleArrayRep, virtualBottom);
+            } else {
+                connectionGrid.union(singleArrayRep, convertToSingleArray(row + 1, col));
+            }
+        }
+    }
+
+    public int numberOfOpenSites() {
+        int trueCount = 0;
+        for (int i = 1; i < id.length; i++) {
+            if (id[i]) trueCount++;
+        }
+        return trueCount;
+    }
+
+    public boolean isFull(int row, int col) {
+        int singleArrayRep = convertToSingleArray(row, col);
+        return connectionGrid.find(singleArrayRep) == virtualTop;
+    }
+
+    public boolean isOpen(int row, int col) {
+        checkBounds(row, col);
+        return id[convertToSingleArray(row, col)];
+    }
+
+    public boolean percolates() {
+        return connectionGrid.find(virtualBottom) == connectionGrid.find(virtualTop);
+    }
+
+    private void checkBounds(int row, int col) {
+        if (row < 1 || row > gridSize || col < 1 || col > gridSize)
+            throw new IllegalArgumentException("Outside prescribed range");
+    }
+
+    private int convertToSingleArray(int row, int col) {
+        return ((row - 1) * gridSize) + col;
     }
 }
---
+++ week1-percolation/src/PercolationStats.java
@@ -0,0 +1,11 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+
+    public PercolationStats(int n, int trials) {
+        Percolation perc = new Percolation(n);
+
+
+    }
+}
